6155852c4611291779f1e4062742cdda33f0abc1
yinlaenyo <120325015+yinlaenyo@users.noreply.github.com>
2023-03-30 19:40:26 +0630

    Create assignment8.c

---
+++ assignment8.c
@@ -0,0 +1,17 @@
+
+
+#include "stdio.h"
+#include "assignment8.h"
+
+int main(){
+    //space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+    
+
+    return 0;
+}
+//
+// Created by asus on 3/30/2023.
+//
